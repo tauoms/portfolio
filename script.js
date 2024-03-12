eccf5429113d26d6f92fe3c27a2adf38ef0338d7
Tuomas Kohvakka <147260100+tauoms@users.noreply.github.com>
2024-03-12 21:20:41 +0200

    Add mobile menu, adjust section scroll-margin-tops

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const backToTopBtn = document.querySelector('#back-to-top');
+const mobileBtn = document.querySelector('.mobile');
+const navUl = document.querySelector('nav ul');
+const menuItems = document.querySelectorAll('nav ul li a');
 
 const getToTop = () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 } 
 
+const toggleMobMenu = () => {
+    navUl.classList.toggle('responsive')
+  }
 
 const greetingOutput = document.querySelector('#greeting');
 
@@ -28,3 +34,5 @@ function greetVisitor () {
 greetVisitor();
 
 backToTopBtn.addEventListener('click', getToTop);
+mobileBtn.addEventListener('click', toggleMobMenu);
+menuItems.forEach(item => item.addEventListener('click', toggleMobMenu));
